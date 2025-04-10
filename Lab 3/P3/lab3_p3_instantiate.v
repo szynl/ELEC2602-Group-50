4fcab280805dae601f0643e33e23a3fb5766dbe2
Pseudonym-2 <maka.mutyiri@gmail.com>
2025-04-10 13:51:09 +1000

    Update lab3_p3_instantiate.v

--- Lab 3/P3/lab3_p3_instantiate.v
+++ Lab 3/P3/lab3_p3_instantiate.v
@@ -2,7 +2,9 @@ module lab3_p3_instantiate(SW,LEDR);
 
 	input[9:0] SW;
 	output [9:0] LEDR;
+	wire zero=1'b0;
+	
 	
 	//instantiate and connect fourBit_FA
-	
+	fourBit_FA x(.a(SW[4:1]),.b(SW[8:5]),.cin(zero),.cout(LEDR[4]),.s(LEDR[3:0]));
 endmodule
